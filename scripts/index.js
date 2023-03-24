6431cd2ff605d9c0d458e82820511329d1ad93d8
KonstantinTyur <tur-k-v@yandex.ru>
2023-03-24 09:09:36 +0300

    feat: Добавить заполнение 6 карточками страницы

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,27 @@
 
-
+/* ~ по работе с секцией profile */
 const profileBtnEdit = document.querySelector('.profile__edit-button');
 const profileBtnAdd = document.querySelector('.profile__add-button');
 const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
 
-
+/* ~ по выборке popup и кнопок закрыть у всех попапов */
 const popup = document.querySelector('.popup');
 const popupBtnClose = document.querySelectorAll('.popup__close-button');
 
+/* ~ по работе с popup Profile*/
 const popupProfile = document.querySelector('.popup_type_profile');
 const popupFormProfile = popupProfile.querySelector('.popup__form');
 const popupProfileInputName = popupProfile.querySelector('.popup__input-box_type_name');
 const popupProfileInputJob = popupProfile.querySelector('.popup__input-box_type_job');
 
-
-
+/* ~ по работе с popup Card*/
 const popupAddCard = document.querySelector('.popup_type_card');
-const popupAddCardInputLegend = popupAddCard.querySelector('.popup__input-box_type_legend');
+const popupFormCard = popupAddCard.querySelector('.popup__form');
+const popupAddCardInputTitle = popupAddCard.querySelector('.popup__input-box_type_title');
 const popupAddCardInputUrl = popupAddCard.querySelector('.popup__input-box_type_url');
 
-const cardElementTemplate = document.querySelector('#cardElementTemplate').content;
+/* ~ массив с карточками */
 const initialCards = [
   {
     name: 'Архыз',
@@ -48,15 +49,53 @@ const initialCards = [
   }
 ];
 
-function openPopup (popup) {
-    popup.classList.add('popup_opened');
+/* ~ по выбору содержимого тега template */
+const cardElementTemplate = document.querySelector('#cardElementTemplate').content;
 
+/* ~ для выбора места вставки в список ul */
+const cardList = document.querySelector('.elements');
+
+/* f создания карточек */
+function createCard (object) {
+  const cardItem = cardElementTemplate.querySelector('.elements__item').cloneNode(true);
+  const cardTrash = cardItem.querySelector('.element__del-button');
+  const cardImage = cardItem.querySelector('.element__img');
+  const cardCaption = cardItem.querySelector('.element__text');
+  const cardLike = cardItem.querySelector('.element__like');
+
+  cardItem.querySelector('.element').ariaLabel = object.name;
+  cardCaption.textContent = object.name;
+  cardImage.alt = object.name;
+  cardImage.src = object.link;
+
+  cardLike.addEventListener('click', () => {
+    cardLike.classList.toggle('element__like_type_active');
+  });
+
+  cardTrash.addEventListener('click', () => {
+    cardItem.remove();
+  });
+
+  return cardItem;
 }
 
+/* заполнения страницы карточками из массива*/
+initialCards.forEach((element) => {
+  const card = createCard(element);
+  cardList.append(card);
+})
+
+/* f открытия любого popup */
+function openPopup (popup) {
+    popup.classList.add('popup_opened');
+}
+
+/* f закрытия любого popup */
 function closePopup (popup) {
     popup.classList.remove('popup_opened');
 }
 
+/* f редактирования данных на странице через popup Profile */
 function handleFormProfileSubmit (evt) {
     evt.preventDefault();
     profileTitle.textContent =  popupProfileInputName.value;
@@ -64,28 +103,30 @@ function handleFormProfileSubmit (evt) {
     closePopup(popupProfile);
 }
 
+/* ???? f добавления карточек на страницу через popup Card */
 function handleFormAddCardSubmit (evt) {
   evt.preventDefault();
-}
+  cardList.append(createCard({name: popupProfileInputName.value, link: popupAddCardInputUrl.value}));
+  debugger;
+  closePopup(popupAddCard);
+  };
 
 
 
+
+/* Слушатель клика на кнопку редактирования персональных данных  в секции Profile */
 profileBtnEdit.addEventListener('click', () => {
   popupProfileInputName.value = profileTitle.textContent;
   popupProfileInputJob.value = profileDescription.textContent;
   openPopup(popupProfile);
 });
 
+/* ???? Слушатель клика на кнопку добавления карточек в секции Card */
 profileBtnAdd.addEventListener('click', () => {
-  popupAddCardInputLegend.value =
-
   openPopup(popupAddCard);
-})
+});
 
-
-
-
-
+/* Слушатель клика на кнопку крестик в любом popup */
 popupBtnClose.forEach((element) => {
   const popup = element.closest('.popup');
   element.addEventListener('click', () => closePopup(popup));
